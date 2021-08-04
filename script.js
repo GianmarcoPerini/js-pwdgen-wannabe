dc4d80bf8c3e3e535dbb3845704ff44cdc3de742
GianmarcoPerini <g.marco.perini@gmail.com>
2021-08-04 15:24:34 +0200

    pw-generator

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("prova")
+let nome = prompt("Tuo nome:");
+let cognome = prompt("Tuo congnome:");
+let colore = prompt("Il tuo colore preferito in assoluto:");
+
+document.getElementById("new-pw").innerHTML = nome + cognome + colore + 21;
